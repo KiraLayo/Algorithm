343920853fc6b1978f534f79943bf0c5551b40cf
qizheng.wen <qizheng.wen@yeepay.com>
2021-03-31 19:49:42 +0800

    no message

---
+++ 873. 最长的斐波那契子序列的长度/main.swift
@@ -0,0 +1,71 @@
+//
+//  main.swift
+//  873. 最长的斐波那契子序列的长度
+//
+//  Created by yp-tc-m-2548 on 2021/3/31.
+//  Copyright © 2021 KiraLayo. All rights reserved.
+//
+//873. 最长的斐波那契子序列的长度
+//如果序列 X_1, X_2, ..., X_n 满足下列条件，就说它是 斐波那契式 的：
+//
+//n >= 3
+//对于所有 i + 2 <= n，都有 X_i + X_{i+1} = X_{i+2}
+//给定一个严格递增的正整数数组形成序列，找到 A 中最长的斐波那契式的子序列的长度。如果一个不存在，返回  0 。
+//
+//（回想一下，子序列是从原序列 A 中派生出来的，它从 A 中删掉任意数量的元素（也可以不删），而不改变其余元素的顺序。例如， [3, 5, 8] 是 [3, 4, 5, 6, 7, 8] 的一个子序列）
+//
+//
+//
+//示例 1：
+//
+//输入: [1,2,3,4,5,6,7,8]
+//输出: 5
+//解释:
+//最长的斐波那契式子序列为：[1,2,3,5,8] 。
+//示例 2：
+//
+//输入: [1,3,7,11,12,14,18]
+//输出: 3
+//解释:
+//最长的斐波那契式子序列有：
+//[1,11,12]，[3,11,14] 以及 [7,11,18] 。
+//
+//
+//提示：
+//
+//3 <= A.length <= 1000
+//1 <= A[0] < A[1] < ... < A[A.length - 1] <= 10^9
+//（对于以 Java，C，C++，以及 C# 的提交，时间限制被减少了 50%）
+
+import Foundation
+
+class Solution {
+    func lenLongestFibSubseq(_ arr: [Int]) -> Int {
+//        DP
+//        dp[i][j] 以i为为一个元素，以j为结尾元素的子序列的最长长度
+        var map: [Int:Int] = [:];
+        let N = arr.count
+        for i in 0..<arr.count {
+            map[arr[i]] = i;
+        }
+        var dp:[Int:Int] = [:];
+        
+        var res = 0;
+        for i in 0..<N {
+            for j in i+1..<N {
+                let diff = arr[j] - arr[i];
+                if diff >= arr[i] {
+                    break;
+                }
+                if let diffIndex = map[diff] {
+                    dp[i*N + j] = dp[diffIndex * N + i, default: 2] + 1;
+                    res = max(dp[i*N + j, default: 2], res);
+                }
+            }
+        }
+        return res > 2 ? res : 0;
+    }
+}
+
+print(Solution().lenLongestFibSubseq([1,2,3,4,5,6,7,8]))
+print(Solution().lenLongestFibSubseq([1,3,7,11,12,14,18]))
